c9dd167ed621db0eee7d2456802aa644088e2eed
Judith Uchechukwu Ezenwosu <88844700+Uceeyjudy@users.noreply.github.com>
2022-02-27 18:37:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// Get a hold of the element with Id today-date
+const dateElement =document.getElementById("today-date");
+// Set the function (Inner HTML) to be a string representation of today's date
+dateElement.innerHTML = new Date( );
